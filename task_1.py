4f9cb8ade62441490dbdd0a6f0b92b4f5ac20fb9
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-16 12:31:26 +0200

    Added license plate validation

--- task_1.py
+++ task_1.py
@@ -1,6 +1,11 @@
-def list_numbers(numbers):
-   result = [n for n in numbers if n % 2 == 0 and n != 0]
+def clean_plates(plates):
+   result = []
+   for i in plates:
+      i = str(i).upper().strip()
+      if len(i) == 8:
+         result.append(i)
+         
    return result
-    
-numbers = [1, 2, 0, 4, 5, 0, 8]
-print(list_numbers(numbers))
+      
+plates = ["  aa1234bb ", "invalid_plate", "XX9999XX", " a b c ", "kH0mycH1"]
+print(clean_plates(plates))
