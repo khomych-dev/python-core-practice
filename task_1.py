32a4420d1ff7abce6256fd2d42018b5baa6c9523
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-16 17:53:02 +0200

    Added car registration to the dictionary

--- task_1.py
+++ task_1.py
@@ -1,11 +1,19 @@
 def clean_plates(plates):
    result = []
-   for i in plates:
-      i = str(i).upper().strip()
-      if len(i) == 8:
-         result.append(i)
+   for plate in plates:
+      clean_plate = str(plate).upper().strip()
+      if 3 <= len(clean_plate) <= 8:
+         result.append(clean_plate)
          
    return result
+
+def register_cars(clean_plates, status="in repair"):
+   my_garage = {}
+   for plat in clean_plates:
+      my_garage[plat] = status
       
+   return my_garage
+   
 plates = ["  aa1234bb ", "invalid_plate", "XX9999XX", " a b c ", "kH0mycH1"]
-print(clean_plates(plates))
+cleaned_data = clean_plates(plates)
+print(register_cars(cleaned_data))
