6cbd290b7a23e7b9e05e6255cb43881c3318a0de
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-17 10:44:31 +0200

    Added a while loop

--- task_1.py
+++ task_1.py
@@ -17,14 +17,22 @@ def register_cars(clean_plates, status="in repair"):
 def release_car(garage_db, plate_number):
    if plate_number in garage_db:
       del garage_db[plate_number]
-      print("The car has been successfully issued")
+      print(f"The car {plate_number} has been successfully issued")
       return garage_db
       
    print(f"The car plate_number {plate_number} was not found")
    return garage_db
-   
+
 plates = ["  aa1234bb ", "invalid_plate", "XX9999XX", " a b c ", "kH0mycH1"]
 cleaned_data = clean_plates(plates)
 garage_db = register_cars(cleaned_data)
+
+while True:
+   action = input("Enter the command: ").lower()
+   if action == 'return the car':
+      plate_num = input("Enter your license plate number: ").upper()
+      if plate_num in garage_db:
+         release_car(garage_db, plate_num)
+         break
+         
 print(garage_db)
-print(release_car(garage_db, 'KH0MYCH1'))
