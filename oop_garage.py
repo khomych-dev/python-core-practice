490ba9aafd1fac46cbdb13aa3245a1425890ea3b
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-18 19:37:27 +0200

    Added the 'save' method

--- oop_garage.py
+++ oop_garage.py
@@ -1,6 +1,11 @@
 from task_1 import load_garage
+from task_1 import save_garage
 
 class Garage:
     def __init__(self, filename='garage.json'):
         self.filename = filename
         self.db = load_garage(self.filename)
+
+        
+    def save(self):
+        save_garage(self.db, self.filename)
