163d286159f0472f76fee5d78c49a714da8ad7a7
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-19 13:11:04 +0200

    Added the '_load_garage' method

--- oop_garage.py
+++ oop_garage.py
@@ -1,12 +1,20 @@
-from task_1 import load_garage
 from task_1 import save_garage
-from task_1 import clean_plates
+import json
 
 class Garage:
     def __init__(self, filename='garage.json'):
         self.filename = filename
-        self.db = load_garage(self.filename)
+        self.db = self._load_garage()
 
+    def _load_garage(self):
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                return data
+      
+        except FileNotFoundError:
+            return {}
+    
     def save(self):
         save_garage(self.db, self.filename)
         
@@ -18,18 +26,18 @@ class Garage:
         return f"The car plate_number {plate_number} was not found"
     
     def register_car(self, plate_number, status='in repair'):
-        for plate in clean_plates([plate_number]):
+        for plate in self._clean_plates([plate_number]):
             self.db[plate] = status
             self.save()
             return f"The car {plate_number} has been successfully registered"
         return "Invalid plate format. Registration failed."
     
-    def clean_plates(self, plates):
+    def _clean_plates(self, plates):
         result = []
         for plate in plates:
-            clean_plate = str(plate).upper().strip()
-        if 3 <= len(clean_plate) <= 8:
-            result.append(clean_plate)
+            clean_plate = str(plate).upper().strip().replace(" ", "")
+            if 3 <= len(clean_plate) <= 8:
+                result.append(clean_plate)
          
         return result
     
