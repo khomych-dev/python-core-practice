6836577be8fc252e9e01f81b336dfbee0624f6c3
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-19 11:01:26 +0200

    A new method, 'release_car', has been created

--- oop_garage.py
+++ oop_garage.py
@@ -6,6 +6,13 @@ class Garage:
         self.filename = filename
         self.db = load_garage(self.filename)
 
-        
     def save(self):
         save_garage(self.db, self.filename)
+        
+    def release_car(self, plate_number):
+        if plate_number in self.db:
+            del self.db[plate_number]
+            self.save()
+            return f"The vehicle with license plate number {plate_number} has been successfully returned to its owner"
+        
+        return f"The car plate_number {plate_number} was not found"
