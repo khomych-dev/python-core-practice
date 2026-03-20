fa19266901b04a5d443c02a717f193ab0c6d51e8
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-20 20:10:02 +0200

    Fixed bugs in the 'test_register_car_duplicate' function

--- test_garage.py
+++ test_garage.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 from oop_garage import Garage
 
@@ -15,12 +16,11 @@ def test_register_car_success():
 def test_register_car_duplicate():
     test_file = "test_test_db.json"
     garage = Garage(filename=test_file)
-    
-    garage.register_car("AA1111BB")
     garage.register_car("AA1111BB")
     
-    assert "AA1111BB" in garage.db
-    
+    with pytest.raises(ValueError):
+        garage.register_car("AA1111BB")
+        
     if os.path.exists(test_file):
         os.remove(test_file)
     
