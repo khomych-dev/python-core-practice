6698b9e5eae21662ead40af28acdceb6f0f70e96
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-23 18:13:09 +0200

    feat: added a new function, register_new_car

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from oop_garage import Garage
 
 app = FastAPI()
-my_darage = Garage()
+my_garage = Garage()
 
 class CarRegisterRequest(BaseModel):
     plate_number: str
@@ -11,3 +11,13 @@ class CarRegisterRequest(BaseModel):
 @app.get('/')
 def read_root():
     return {"message": "Welcome to the Garage API"}
+
+@app.post("/cars/register")
+def register_new_car(request: CarRegisterRequest):
+    try:
+        result_message = my_garage.register_car(request.plate_number)
+        
+        return {"message": result_message}
+        
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
