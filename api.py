7efa82da4ae0ff8776e05eeecdc3f79aed7b1d88
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-23 11:10:42 +0200

    feat: Added a new read_root function

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from oop_garage import Garage
 
-api = FastAPI()
+app = FastAPI()
 
 my_darage = Garage()
+
+@app.get('/')
+def read_root():
+    return {"message": "Welcome to the Garage API"}
