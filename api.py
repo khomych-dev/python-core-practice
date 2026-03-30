fc7287b5a34522391a4bcf6f65efb28b4e2e0878
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-30 20:30:04 +0300

    feat: created the 'value_error_handler' function

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from oop_garage import Garage
 
@@ -12,6 +13,20 @@ class CarRegisterRequest(BaseModel):
 
 class StatusUpdateRequest(BaseModel):
     new_status: str
+    
+
+@app.exception_handler(ValueError)
+async def value_error_handler(request: Request, exc: ValueError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={
+            "type": "about:blank",
+            "title": "Validation Error",
+            "status": 400,
+            "detail": str(exc),
+            "instance": str(request.url)
+        }
+    )
 
 
 @app.get('/')
@@ -27,33 +42,21 @@ async def all_cars():
 
 @app.post("/cars", status_code=status.HTTP_201_CREATED)
 async def register_new_car(request: CarRegisterRequest):
-    try:
-        result_message = await my_garage.register_car(request.plate_number)
+    result_message = await my_garage.register_car(request.plate_number)
 
-        return {"message": result_message}
-
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    return {"message": result_message}
 
 
 @app.delete("/cars/{plate_number}")
 async def release_car_endpoint(plate_number: str):
-    try:
-        result_message = await my_garage.release_car(plate_number)
+    result_message = await my_garage.release_car(plate_number)
 
-        return {'message': result_message}
-
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    return {'message': result_message}
 
 
 @app.patch("/cars/{plate_number}/status")
 async def new_status(plate_number: str, request: StatusUpdateRequest):
-    try:
-        result_message = await my_garage.change_status(
+    result_message = await my_garage.change_status(
             plate_number, request.new_status)
 
-        return {"message": result_message}
-
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    return {"message": result_message}
