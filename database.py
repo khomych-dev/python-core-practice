61a611e7ae7ac2087ce26557f2338fbd889e24ef
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-31 12:43:32 +0300

    feat: Added a 'brand' column to the 'init_db' function

--- database.py
+++ database.py
@@ -6,7 +6,10 @@ from config import settings
 async def init_db():
     async with aiosqlite.connect(settings.db_path) as db:
         await db.execute("""
-                         CREATE TABLE IF NOT EXISTS cars (plate_number TEXT PRIMARY KEY, status TEXT
+                         CREATE TABLE IF NOT EXISTS cars
+                         (plate_number TEXT PRIMARY KEY,
+                         brand TEXT,
+                         status TEXT
                          )
                          """)
         await db.commit()
