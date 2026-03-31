83907dea4ce7ba202162a542e29c9a1bdf825024
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-31 12:33:27 +0300

    refactor: Added the class MessageResponse and updated the functions

--- routers/cars.py
+++ routers/cars.py
@@ -11,6 +11,10 @@ class CarRegisterRequest(BaseModel):
 
 class StatusUpdateRequest(BaseModel):
     new_status: str
+    
+    
+class MessageResponse(BaseModel):
+    message: str
 
 
 @router.get("/")
@@ -19,21 +23,21 @@ async def all_cars():
     return cars_dict
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=MessageResponse)
 async def register_new_car(request: CarRegisterRequest):
     result_message = await my_garage.register_car(request.plate_number)
 
     return {"message": result_message}
 
 
-@router.delete("/{plate_number}")
+@router.delete("/{plate_number}", response_model=MessageResponse)
 async def release_car_endpoint(plate_number: str):
     result_message = await my_garage.release_car(plate_number)
 
     return {'message': result_message}
 
 
-@router.patch("/{plate_number}/status")
+@router.patch("/{plate_number}/status", response_model=MessageResponse)
 async def new_status(plate_number: str, request: StatusUpdateRequest):
     result_message = await my_garage.change_status(
             plate_number, request.new_status)
