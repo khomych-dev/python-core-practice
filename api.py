792c03c93efca666b1fa01ad945c363e86fafa1a
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-31 19:06:12 +0300

    feat: added RFC 7807 handler for 422 validation errors

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, status, Request
 from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.exceptions import RequestValidationError
 from routers.cars import router as cars_router
 
 app = FastAPI()
@@ -17,6 +18,21 @@ async def value_error_handler(request: Request, exc: ValueError):
             "status": 400,
             "detail": str(exc),
             "instance": str(request.url)
+        }
+    )
+    
+    
+@app.exception_handler(RequestValidationError)
+async def request_validation_error_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+        content={
+            "type": "about:blank",
+            "title": "Validation Error",
+            "status": 422,
+            "detail": str(exc),
+            "instance": str(request.url)
+            
         }
     )
     
