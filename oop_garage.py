ba5657a481400f0b834db9f9008e08167dac1fd6
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-04-01 17:49:59 +0300

    refactor: migrated 'change_status' method to use SQLAlchemy ORM

--- oop_garage.py
+++ oop_garage.py
@@ -62,17 +62,18 @@ class Garage:
             raise ValueError("Invalid plate format.")
         
         clean_plate = clean_plates_list[0]
-        async with aiosqlite.connect(settings.db_path) as db:
-            cursor = await db.execute("SELECT plate_number FROM cars WHERE plate_number = ?", (clean_plate,))
+        async with AsyncSessionLocal() as session:
+            stmt = select(CarDB).where(CarDB.plate_number == clean_plate)
+            result = await session.execute(stmt)
             
-            row = await cursor.fetchone()
-            if row is None:
-                raise ValueError(f"The car plate_number {plate_number} was not found")
+            car = result.scalar_one_or_none()
+            if not car:
+                raise ValueError(f"Car {clean_plate} not found in Garage!")
 
-            await db.execute("UPDATE cars SET status = ? WHERE plate_number = ?", (new_status, clean_plate))
+            car.status = new_status
+            await session.commit()
             
-            await db.commit()
-            return f"The status of the vehicle with license plate number {plate_number}  has been successfully updated"
+            return f"Status of {clean_plate} changed to {new_status}"
     
     async def get_all_cars(self) -> dict[str, dict[str, str]]:
         async with AsyncSessionLocal() as session:
