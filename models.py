65c434842dff43c69b7bc153ca3887ca36801551
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-04-01 11:31:18 +0300

    feat: created a new 'models' file containing the 'Base' and 'CarDB' classes

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class CarDB(Base):
+    __tablename__ = "cars"
+    
+    plate_number: Mapped[str] = mapped_column(String, primary_key=True)
+    brand: Mapped[str] = mapped_column(String)
+    status: Mapped[str] = mapped_column(String)
