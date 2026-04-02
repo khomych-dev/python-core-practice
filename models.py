484fd7c1bd9a14c3321dadcf09c8359324708073
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-04-02 11:43:39 +0300

    feat: added an 'owner' column to the 'cars' table and created a migration

--- models.py
+++ models.py
@@ -8,7 +8,8 @@ class Base(DeclarativeBase):
 
 class CarDB(Base):
     __tablename__ = "cars"
-    
+
     plate_number: Mapped[str] = mapped_column(String, primary_key=True)
     brand: Mapped[str] = mapped_column(String)
     status: Mapped[str] = mapped_column(String)
+    owner: Mapped[str | None] = mapped_column(String, nullable=True)
