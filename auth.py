fa9b3502fdba46f88fff9a99f97492b75a5b73af
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-04-04 13:18:31 +0300

    feat: added the 'get_current_user' function

--- auth.py
+++ auth.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
 from database import AsyncSessionLocal
 from models import UserDB
-from security import get_password_hash, verify_password, create_access_token
+from security import get_password_hash, verify_password, create_access_token, SECRET_KEY, ALGORITHM
 
-from fastapi.security import OAuth2PasswordRequestForm
-
+import jwt
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 class UserCreate(BaseModel):
@@ -63,3 +65,22 @@ async def login_user(
     )
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        username: str | None = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=401, detail="Could not validate credentials")
+
+        return username
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=401, detail="Token expired. Please log in again.")
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=401, detail="Invalid token. Access denied.")
