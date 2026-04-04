b3c16079a647185c56dd98d1b08abf9f822c4522
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-04-04 12:22:45 +0300

    feat: created an asynchronous 'login_user' function

--- auth.py
+++ auth.py
@@ -5,36 +5,61 @@ from sqlalchemy import select
 
 from database import AsyncSessionLocal
 from models import UserDB
-from security import get_password_hash
+from security import get_password_hash, verify_password, create_access_token
+
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
+
 class UserCreate(BaseModel):
     username: str
     password: str
-    
-    
+
+
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
-        
+
+
 @router.post("/register", status_code=201)
 async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     stmt = select(UserDB).where(UserDB.username == user.username)
     result = await db.execute(stmt)
     if result.scalar_one_or_none():
-        raise HTTPException(status_code=400, detail="Username already registered")
-    
+        raise HTTPException(
+            status_code=400, detail="Username already registered")
+
     hashed_pwd = get_password_hash(user.password)
-    
+
     new_user = UserDB(
-        username=user.username, 
-        hashed_password=hashed_pwd, 
+        username=user.username,
+        hashed_password=hashed_pwd,
         role="mechanic"
     )
-    
+
     db.add(new_user)
     await db.commit()
-    
+
     return {"message": f"Mechanic {user.username} registered successfully!"}
+
+
+@router.post("/login")
+async def login_user(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: AsyncSession = Depends(get_db)
+):
+    stmt = select(UserDB).where(UserDB.username == form_data.username)
+    result = await db.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+
+    access_token = create_access_token(
+        data={"sub": user.username, "role": user.role}
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
