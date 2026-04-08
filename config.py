463fa39f334cf9a0da5bad4f68ec3bdf92f62819
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-04-08 12:16:39 +0300

    feat: added the line 'database_url_test'

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
     secret_key: str
-    model_config = SettingsConfigDict(env_file=".env")
-
+    database_url_test: str | None = None
+    model_config = SettingsConfigDict(env_file=".env", extra="forbid")
 
 settings = Settings()  # type: ignore
