588b788cdb1ae4941e1a2aeafdb36275938e0251
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-04-08 11:55:24 +0300

    test: set up an asynchronous pytest environment and a test-isolated database environment

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+import os
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from dotenv import load_dotenv
+
+from models import Base
+from auth import get_db
+from api import app
+
+load_dotenv()
+
+TEST_DB_URL = os.getenv("DATABASE_URL_TEST")
+
+if TEST_DB_URL is None:
+    raise ValueError(
+        "CRITICAL ERROR: DATABASE_URL_TEST not found in the .env file!")
+
+engine_test = create_async_engine(TEST_DB_URL)
+TestingSessionLocal = async_sessionmaker(
+    engine_test, class_=AsyncSession, expire_on_commit=False)
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def setup_test_db():
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+async def override_get_db():
+    async with TestingSessionLocal() as session:
+        yield session
+
+app.dependency_overrides[get_db] = override_get_db
