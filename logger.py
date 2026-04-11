59dec7a25ba27077527f1b3e1f5ef13f29378797
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-04-11 13:24:56 +0300

    feat: added 'request_id' context processor to logger

--- logger.py
+++ logger.py
@@ -1,6 +1,14 @@
 import logging
 import sys
+from contextvars import ContextVar
 import structlog
+
+request_id_var: ContextVar[str] = ContextVar("request_id", default="unknown")
+
+
+def add_request_id(_logger, _method_name, event_dict):
+    event_dict["request_id"] = request_id_var.get()
+    return event_dict
 
 
 def setup_logging():
@@ -13,6 +21,7 @@ def setup_logging():
     structlog.configure(
         processors=[
             structlog.contextvars.merge_contextvars,
+            add_request_id,
             structlog.stdlib.add_log_level,
             structlog.stdlib.add_logger_name,
             structlog.processors.TimeStamper(fmt="iso"),
